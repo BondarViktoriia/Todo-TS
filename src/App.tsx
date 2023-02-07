dd298426d384206359142a95a7330c9e4edcfb11
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-02-07 20:45:38 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Container,Title,Todo } from "./App.styled";
 import { IItem } from "./types/todo";
 
 const App: React.FC = () => {
-  const [todos, setTodos] = useState<IItem[]>([]
+  const [todos, setTodos] = useState<IItem[]>(JSON.parse(localStorage.getItem("todos") || " ")
 );
   
   useEffect(() => {
